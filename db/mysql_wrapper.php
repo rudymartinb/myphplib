b661551abc200b874d174235895b37d8da02dd93
rudymartinb <paraespam541@gmail.com>
2018-04-23 10:08:28 -0300

    pruebas sobre db wrapper

--- db/mysql_wrapper.php
+++ db/mysql_wrapper.php
@@ -1,51 +1,69 @@
 <?php
 
-namespace mylib;
-
-class mysql_wrapper {
-	private static $db = null;
-	function mysql_wrapper(){
-	}
+namespace mylib {
 	
-	function abrirDefault( $catalogo = "distdev" ){
-		$host = "127.0.0.1"; 
-		$user = "root";
-		$pwd = "sunpei42";
-		$port = 3306;
-		$this->abrir( $host, $user, $pwd, $catalogo, $port );		
-	}
-	
-	function abrir( $host, $user, $pwd, $catalogo, $port ){
-		try{
-			self::$db = new \mysqli( $host, $user, $pwd, $catalogo, $port );	
-		} catch( Exception $e ) {
-			throw new \Exception( $e->getMessage() );
-		}
-	}
+	interface proveedor_sql {
+		public function abrirDefault();
+		public function abrir( $host, $user, $pwd, $catalogo, $port );
+		public function cerrar( );
+		public function ejecutar( $query );
+	}	
 
-	function cerrar( ){
-		self::$db->close();
-		self::$db = null;
-	}
-	
-	function ejecutar( $query ){
-		if( self::$db === null )
-			throw new Exception( "Conexion cerrada!" );
-			
-		try{
-			$res = self::$db->query( $query, MYSQLI_STORE_RESULT );
-		} catch( Exception $e ) {
-			throw new \Exception( $e->message.' --> '. $e->getTraceAsString() );
+	class mysql_wrapper implements proveedor_sql {
+		private static $db = null;
+		private static $conexiones = 0;
+		
+		function mysql_wrapper(){
+		}
+		
+		function get_conexiones(){
+			return self::$conexiones;
+		}
+		
+		function abrirDefault( $catalogo = "distdev" ){
+			$host = "127.0.0.1"; 
+			$user = "root";
+			$pwd = "sunpei42";
+			$port = 3306;
+			$this->abrir( $host, $user, $pwd, $catalogo, $port );		
+		}
+		
+		function abrir( $host, $user, $pwd, $catalogo, $port ){
+			if( self::$db !== null ){
+				return;
+			}
+			try {
+				self::$db = new \mysqli( $host, $user, $pwd, $catalogo, $port );	
+			} catch( Exception $e ) {
+				throw new \Exception( $e->getMessage() );
+			}
 		}
 
-		if( self::$db->errno != 0 ){
-			throw new \Exception( self::$db->error );
+		/* conviene cerrar unicamente si es estrictamente necesario
+		*/
+		function cerrar( ){
+			self::$db->close();
+			self::$db = null;
 		}
-		if( gettype( $res ) == "boolean" )
-			return $res ;		
+		
+		function ejecutar( $query ){
+			if( self::$db === null )
+				throw new \Exception( "Conexion cerrada!" );
+				
+			try{
+				$res = self::$db->query( $query, MYSQLI_STORE_RESULT );
+			} catch( Exception $e ) {
+				throw new \Exception( $e->message.' --> '. $e->getTraceAsString() );
+			}
 
-		return $res->fetch_all( MYSQLI_ASSOC );
+			if( self::$db->errno != 0 ){
+				throw new \Exception( self::$db->error );
+			}
+			if( gettype( $res ) == "boolean" )
+				return $res ;		
+
+			return $res->fetch_all( MYSQLI_ASSOC );
+		}
 	}
 }
-
 ?>
---
+++ test/mysql_wrapper_Test.php
@@ -0,0 +1,189 @@
+<?php
+// require( "config.php" );
+// require_once( $DIST.$CLASS.$DEMO."/cFakeDB.php" );
+require_once( "db/mysql_wrapper.php" );
+error_reporting(E_ALL);
+
+/*
+ * algun dia voy a tener q parar de armar wrappers de mysql
+ * 
+ * que pretendo con la clase de objeto
+ * 
+ * 1) no tener que lidiar con rutinas internas de mysql
+ * 2) delegar totalmente el tratamiento de errores (is this wise?)
+ * 3) no tener q andar pasando un  objeto conexion_a_db a todos lados
+ * 4) hacer una coinsulta y recibir todos los datos como array
+ * 5) hacer una subclase como testdouble  (IMPORTANTE ?!)
+ * 
+*/
+
+class mysql_wrapper_Test extends PHPUnit_Framework_TestCase {
+	
+     
+	public function test_abrirDefault_mal_catalogo(){
+		$error_esperado = "mysqli::mysqli(): (HY000/1049): Unknown database 'no_existe'";
+		$db = new mylib\mysql_wrapper();
+		
+		try {
+			$db->abrirDefault( "no_existe" );
+		} catch ( Exception $e ){
+			$error_actual = $e->getMessage();
+		}
+		$this->assertEquals( $error_esperado, $error_actual, "deberia dar error por no existir el catalogo " );
+		
+	}
+
+	public function test_select_uno(){
+		$db = new mylib\mysql_wrapper();
+        $db->abrirDefault( "testing" );
+		$arr = $db->ejecutar( "SELECT 'sarasa estuvo aqui' as uno" );
+		
+		$this->assertEquals( $arr[0]['uno'] , "sarasa estuvo aqui" );
+		// $db->cerrar();
+	}
+	
+
+	public function test_no_existe_tabla(){
+		
+		$error_esperado = "Table 'testing.sarasa' doesn't exist";
+		
+		$db = new mylib\mysql_wrapper();
+		$db->abrirDefault( "testing" );
+		
+		try {
+			$arr = $db->ejecutar( "SELECT * from sarasa" );
+		} catch( Exception $x ) {
+			$error_actual = $x->getMessage();
+		}
+		$this->assertEquals( $error_esperado, $error_actual, "no deberia existir tabla sarasa " );
+
+		
+        // $db->cerrar();
+		
+	}
+	
+
+	public function test_SelectVacio(){
+		$db = new mylib\mysql_wrapper();
+		$db->abrirDefault( "testing" );
+
+		$arr = $db->ejecutar( "SELECT * from tabla1 where false" );
+		$this->assertEquals( "array", gettype( $arr )  );
+		$this->assertEquals( 0, count( $arr )  );
+		
+		// $db->cerrar();
+	}	
+	
+	public function test_InsertOK(){
+		$db = new mylib\mysql_wrapper( );
+		$db->abrirDefault( "testing" );
+
+		$db->ejecutar( "start transaction;" );
+		$db->ejecutar( "delete from tabla1;" );
+		$db->ejecutar( "insert into tabla1 set sarasa = 'queseyo'" );
+		
+		$arr = $db->ejecutar( "SELECT sarasa from tabla1" );
+		
+		$this->assertEquals( "queseyo", $arr[0]['sarasa'], "valor insertado en el registro" );
+
+		$arr = $db->ejecutar( "rollback;" );
+		// $db->cerrar();
+
+	}	
+	
+	/*
+CREATE TABLE  `testing`.`relacionada1` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `elotroid` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_relacionada1_1` (`elotroid`),
+  CONSTRAINT `FK_relacionada1_1` FOREIGN KEY (`elotroid`) REFERENCES `relacionada2` (`elotroid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='dependiente';
+	*/
+	
+	/* 
+	 * la idea es generar un error de sql mediante una falla de integridad de SQL
+	*/
+	public function test_insert_error(){
+		$error_esperado = "Cannot add or update a child row: a foreign key constraint fails (`testing`.`relacionada1`, CONSTRAINT `FK_relacionada1_1` FOREIGN KEY (`elotroid`) REFERENCES `relacionada2` (`elotroid`))";
+		
+		$db = new mylib\mysql_wrapper();
+		$db->abrirDefault( "testing" );
+
+		$query = "insert into relacionada1 set elotroid = 0";
+		
+		try {
+			$arr = $db->ejecutar( $query );
+		} catch( Exception $e ) {
+			$error_actual = $e->getMessage();
+		}
+		$this->assertEquals( $error_esperado, $error_actual, "cuando se inserta un registro en una dependiente de dos tablas relacionadas, y no tiene el id correcto y debe fallar" );
+
+		// $db->cerrar();
+	}	
+
+	
+	public function test_insert_falla(){
+		$error_esperado = "Column 'sarasa' cannot be null";
+		
+		$db = new mylib\mysql_wrapper(  );
+		$db->abrirDefault( "testing" );
+		
+		try {
+			$arr = $db->ejecutar( "insert into tabla1 set sarasa = null" );
+		} catch( Exception $e ) {
+			$error_actual = $e->getMessage();
+		}
+		$this->assertEquals( $error_esperado, $error_actual, "cuando se inserta un registro con null deberia fallar" );
+
+		// $db->cerrar();
+	}	
+	
+	public function test_update_ok(){
+		$db = new mylib\mysql_wrapper( );
+		$db->abrirDefault( "testing" );
+		
+		$db->ejecutar( "start transaction;" );
+		$db->ejecutar( "delete from tabla1;" );
+		$db->ejecutar( "insert into tabla1 set sarasa = 'otra'" );
+		$db->ejecutar( "update tabla1 set sarasa = 'queseyo'" );
+		
+		$arr = $db->ejecutar( "SELECT sarasa from tabla1" );
+		
+		$this->assertEquals( "queseyo", $arr[0]['sarasa'], "valor actualizado en el unico registro" );
+
+		$arr = $db->ejecutar( "rollback;" );
+		// $db->cerrar();
+
+	}
+	
+	public function test_update_error(){
+		$error_esperado = "Cannot add or update a child row: a foreign key constraint fails (`testing`.`relacionada1`, CONSTRAINT `FK_relacionada1_1` FOREIGN KEY (`elotroid`) REFERENCES `relacionada2` (`elotroid`))";
+		
+		$db = new mylib\mysql_wrapper();
+		$db->abrirDefault( "testing" );
+		
+		$db->ejecutar( "insert into relacionada2 set algo = 'algo2'" );
+		$arr = $db->ejecutar( "select last_insert_id() as id" );
+		$db->ejecutar( "insert into relacionada1 set elotroid = ".$arr[0]['id'] );
+
+		try {
+			$arr = $db->ejecutar( "update relacionada1 set elotroid = 0" );
+		} catch( Exception $e ) {
+			$error_actual = $e->getMessage();
+		}
+		$this->assertEquals( $error_esperado, $error_actual, "dos tablas relacionadas, se inserta un registro en una dependiente que no tiene el id correcto, debe fallar" );
+
+		// $db->cerrar();
+	}	
+
+	/* si convertimos la static en public static podemos probar esto
+	function test_Mismo_Obj(){
+		$db = new mylib\mysql_wrapper( "distdev" );
+		$db2 = new mylib\mysql_wrapper( "distdev" );
+		$this->assertTrue( $db::$db === $db2::$db );
+	}
+	*/
+
+}
+?>
