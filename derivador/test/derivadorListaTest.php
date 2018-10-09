0923ef8ea6fddab229fb4c48d9322c6757d70c44
rudymartinb <paraespam541@gmail.com>
2018-10-09 11:49:38 -0300

    derivador

---
+++ derivador/test/derivadorListaTest.php
@@ -0,0 +1,37 @@
+<?php
+use derivador\derivadorLista;
+
+/**
+ * derivadorItem test case.
+ */
+class derivadorListaTest extends PHPUnit\Framework\TestCase {
+    function testNew(){
+        $url = "/sarasa";
+        $funcion = function(){
+          return true;  
+        };
+        
+        $lista = new derivadorLista();
+        $lista->agregar( $url, $funcion  );
+        
+        $this->assertTrue( $lista->existe( $url ) );
+        
+    }
+    
+    function testFuncion(){
+        $url = "/sarasa";
+        $funcion = function(){
+            return true;
+        };
+        
+        $lista = new derivadorLista();
+        $lista->agregar( $url, $funcion  );
+        
+        $ret = $lista->funcion( $url );
+        $this->assertTrue( is_callable( $ret ) );
+        
+        
+    }
+    
+}
+
---
+++ derivador/test/derivadorTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use derivador\Derivador;
+
+/**
+ * derivador test case.
+ */
+class derivadorTest extends PHPUnit\Framework\TestCase
+{
+
+    function testSarasa(){
+        $este = $this;
+        $url = "/sarasa"; 
+        $funcion = function() use ($este){ $este->assertTrue( true );  };
+        
+        $derivador = new Derivador();
+        
+        $derivador->agregar( $url, $funcion );
+        $derivador->ejecutar( $url );
+    }
+    
+    function testFail(){
+        $derivador = new Derivador();
+        
+        $derivador->ejecutar( "/nada" );
+        $this->assertTrue( true );
+    }
+    
+
+}
+
