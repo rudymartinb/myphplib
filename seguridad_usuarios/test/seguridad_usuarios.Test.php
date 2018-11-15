083ec11a3d466c596634d23eadd29f20bcf338a1
rudymartinb <paraespam541@gmail.com>
2018-11-15 12:48:17 -0300

    pruebas sobre Menu con seguridad de usuarios incluida

--- seguridad_usuarios/test/seguridad_usuarios.Test.php
+++ seguridad_usuarios/test/seguridad_usuarios.Test.php
@@ -1,22 +1,106 @@
 <?php
+
 class Menu {
+    private $primarios;
+    private function __construct(){
+        $this->primarios = [];
+    }
+    
     static function Builder() {
-        return new class {
-            function Agregar(){
-                return $this;
-                
+        $menu = new Menu();
+        return $menu;
+    }
+    
+    function agregarPri( string $opcion ){
+        $pri = new Pri( $this, $opcion );
+        $this->primarios[] = $pri;
+        return $pri;
+    }
+    
+    function ejecutar( $tag ){
+        foreach ($this->primarios as $pri ){
+            $pri->ejecutar_x_tag($tag);
+        }
+    }
+}
+
+class Pri extends Menu {
+    private $menu;
+    private $nombre;
+    private $subs;
+    function __construct( Menu $menu, string $opcion ){
+        $this->menu = $menu;
+        $this->nombre = $opcion;
+        $this->subs = [];
+    }
+    
+    function ejecutar_x_tag( $tag ){
+        foreach ($this->subs as $sub ){
+            if( $sub->get_tag() === $tag  ){
+                $funcion = $sub->get_funcion();
+                $funcion();
             }
-            function AgregarSub(){
-                return $this;
-            }
-            function build(){
-                
-            }
-        };
+        }
+    }
+    
+    function AgregarSub( string $nombre, string $tag, Array $grupos, string $fuente, callable $funcion ){
+        $this->subs[] = new Sec( $nombre, $tag, $grupos, $fuente, $funcion  ) ;
+        return $this;
+    }
+    
+    function buildPri(){
+        return $this->menu;
     }
     
 }
+
+class Sec extends Pri {
+    private $nombre;
+    private $tag;
+    private $grupos;
+    private $funcion;
+    private $fuente;
+    function __construct( string $nombre, string $tag, Array $grupos, string $fuente, callable $funcion ){
+        $this->nombre = $nombre;
+        $this->tag = $tag;
+        $this->grupos = $grupos;
+        $this->fuente = $fuente;
+        $this->funcion = $funcion;
+    }
+    function get_nombre(){
+        return $this->nombre;
+    }
+    function get_tag(){
+        return $this->tag;
+    }
+    function get_grupos(){
+        return $this->grupos;
+    }
+    function get_funcion(){
+        return $this->funcion;
+    }
+    function get_fuente(){
+        return $this->fuente;
+    }
+    
+}
+
 class seguridad_usuariosTest extends PHPUnit\Framework\TestCase {
+    
+    function test_ejecutar(){
+        $funcion = function() {
+            $this->assertTrue( true );
+        };
+        
+        // prueba funcional
+        $menu = Menu::Builder() // devuelve un nuevo Menu()
+        ->AgregarPri( "Clientes" ) // Devuelve un Pri
+        ->AgregarSub( "Agregar Clientes", "agregar_clientes", [ "Autorizados","Administradores","Operadores" ], "fuente", $funcion )
+        ->buildPri(); // devuelve el Menu actual
+        
+        $menu->ejecutar( "agregar_clientes" );
+    }
+
     function test_New(){
         /*
          * la idea de esta clase de objeto es establecer una relacion 
@@ -31,37 +115,30 @@ class seguridad_usuariosTest extends PHPUnit\Framework\TestCase {
          * nombre submenu + opcion + grupos habilidatos con comas
          * 
          */
-        $arr = [];
-                
-        $arr[ "Clientes" ] = [ ];
-        $arr[ "Clientes" ]["agregar_clientes"]["nombre"] = "Agregar Clientes"  ;
-        $arr[ "Clientes" ]["agregar_clientes"]["grupos"] = [ "Autorizados","Administradores","Operadores" ] ;
-        
 
+        $funcion = function(){ $this->assertTrue(true); };
+        
         // prueba funcional
-        $menu = Menu::Builder()->Agregar( "Clientes" )
-        ->AgregarSub( "Agregar Clientes", "agregar_clientes", [ "Autorizados","Administradores","Operadores" ] )
-        ->AgregarSub( "Modificar Clientes", "modificar_clientes", [ "Autorizados","Administradores","Operadores" ] )
-        ->AgregarSub( "Borrar Clientes", "borrar_clientes", [ "Autorizados","Administradores","Operadores" ] )
-        ->build();
+        $menu = Menu::Builder() // devuelve un nuevo Menu()
+        ->AgregarPri( "Clientes" ) // Devuelve un Pri
+        ->AgregarSub( "Agregar Clientes", "agregar_clientes", [ "Autorizados","Administradores","Operadores" ], "fuente", $funcion )
+        ->AgregarSub( "Modificar Clientes", "modificar_clientes", [ "Autorizados","Administradores","Operadores" ] , "fuente", $funcion )
+        ->AgregarSub( "Borrar Clientes", "borrar_clientes", [ "Autorizados","Administradores","Operadores" ] , "fuente", $funcion )
+        ->buildPri() // devuelve el Menu actual
+        ->AgregarPri( "Proveedores" ) 
+        ->AgregarSub( "Agregar Proveedores", "agregar_prov", [ "Autorizados","Administradores","Operadores" ] , "fuente", $funcion )
+        ->AgregarSub( "Modificar Proveedores", "modificar_prov", [ "Autorizados","Administradores","Operadores" ] , "fuente", $funcion )
+        ->AgregarSub( "Borrar Proveedores", "borrar_prov", [ "Autorizados","Administradores","Operadores" ] , "fuente", $funcion )
+        ->buildPri();
         
+        $menu->ejecutar( "borrar_prov" );
         
+//         $json = json_encode( $arr );
+//         $esperado = '{"Clientes":{"Agregar Clientes":"agregar_clientes","Modificar Clientes":"modificar_clientes","Borrar Clientes":"eliminar_clientes"}}';
+//         $actual = $json;
         
-        $json = json_encode( $arr );
-        var_dump( $json );
-        
-        var_dump( json_decode( $json , true ) );
-        
-        $esperado = '{"Clientes":{"Agregar Clientes":"agregar_clientes","Modificar Clientes":"modificar_clientes","Borrar Clientes":"eliminar_clientes"}}';
-        
-        $actual = $json;
-        
-        // $this->assertEquals( $esperado, $actual );
-        
-        
-        
-        // $arender = separar_grupos( $arr  );
-        
+        // por ahora lo dejo asi
+        $this->assertTrue( true );
         
         
     }
