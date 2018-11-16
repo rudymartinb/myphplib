962ae646951b238740cd038954b9d17048834f87
rudymartinb <paraespam541@gmail.com>
2018-11-16 10:42:53 -0300

    seguridad usuarios

--- seguridad_usuarios/test/seguridad_usuarios.Test.php
+++ seguridad_usuarios/test/seguridad_usuarios.Test.php
@@ -2,6 +2,8 @@
 
 class Menu {
     private $primarios;
+    private $defaultFun;
+    
     private function __construct(){
         $this->primarios = [];
     }
@@ -10,8 +12,8 @@ class Menu {
         $menu = new Menu();
         return $menu;
     }
-    
-    function agregarPri( string $opcion ){
+     
+    function agregarPrimario( string $opcion ){
         $pri = new Pri( $this, $opcion );
         $this->primarios[] = $pri;
         return $pri;
@@ -22,20 +24,29 @@ class Menu {
             $pri->ejecutar_x_tag($tag);
         }
     }
+    function existe_fuente( $fuente ){
+        return file_exists( $fuente );
+    }
+    
+    function funcionDefault( callable $fun ){
+        $this->defaultFun = $fun;
+        return $this;
+    }
+    
 }
 
 class Pri extends Menu {
     private $menu;
     private $nombre;
-    private $subs;
+    private $secundarios;
     function __construct( Menu $menu, string $opcion ){
         $this->menu = $menu;
         $this->nombre = $opcion;
-        $this->subs = [];
+        $this->secundarios = [];
     }
     
     function ejecutar_x_tag( $tag ){
-        foreach ($this->subs as $sub ){
+        foreach ($this->secundarios as $sub ){
             if( $sub->get_tag() === $tag  ){
                 $funcion = $sub->get_funcion();
                 $funcion();
@@ -43,29 +54,31 @@ class Pri extends Menu {
         }
     }
     
-    function AgregarSub( string $nombre, string $tag, Array $grupos, string $fuente, callable $funcion ){
-        $this->subs[] = new Sec( $nombre, $tag, $grupos, $fuente, $funcion  ) ;
-        return $this;
+    function AgregarSecundario( ){
+        $sec = Sec::BuilderSec( $this );
+        return $sec;
+    }
+    protected function addsec( Sec $sec ){
+        $this->secundarios[] = $sec ;
     }
     
-    function buildPri(){
+    function buildMenu(){
         return $this->menu;
     }
     
 }
 
 class Sec extends Pri {
-    private $nombre;
-    private $tag;
-    private $grupos;
-    private $funcion;
-    private $fuente;
-    function __construct( string $nombre, string $tag, Array $grupos, string $fuente, callable $funcion ){
-        $this->nombre = $nombre;
-        $this->tag = $tag;
-        $this->grupos = $grupos;
-        $this->fuente = $fuente;
-        $this->funcion = $funcion;
+    protected $nombre;
+    protected $tag;
+    protected $grupos;
+    protected $funcion;
+    protected $fuente;
+    
+    protected function __construct(  ){
+    }
+    static function BuilderSec( $pri ){
+        return new BuilderSecundario( $pri );
     }
     function get_nombre(){
         return $this->nombre;
@@ -85,6 +98,44 @@ class Sec extends Pri {
     
 }
 
+class BuilderSecundario extends Sec {
+    private $pri ;
+    public function __construct( Pri $pri ){
+        $this->pri = $pri;
+    }
+    
+    function nombre( string $nombre ){
+        $this->nombre = $nombre;
+        return $this;
+    }
+    function tag( string $nombre ){
+        $this->tag = $nombre;
+        return $this;
+    }
+    function grupos( Array $nombre ){
+        $this->grupos = $nombre;
+        return $this;
+    }
+    function fuente( string $nombre ){
+        $this->fuente = $nombre;
+        return $this;
+    }
+    function funcion( callable $nombre ){
+        $this->funcion = $nombre;
+        return $this;
+    }
+    function buildOpcion() : Pri {
+        $sec = new Sec();
+        $sec->nombre = $this->nombre;
+        $sec->tag = $this->tag;
+        $sec->grupos = $this->grupos;
+        $sec->fuente  = $this->fuente;
+        $sec->funcion  = $this->funcion;
+        $this->pri->addSec( $sec );
+        return $this->pri;
+    }
+}
+
 class seguridad_usuariosTest extends PHPUnit\Framework\TestCase {
     
     function test_ejecutar(){
@@ -92,53 +143,98 @@ class seguridad_usuariosTest extends PHPUnit\Framework\TestCase {
             $this->assertTrue( true );
         };
         
+        /*
+         * HONESTAMENTE queda mucho mas largo
+         * pero si la idea es comunicar con claridad,
+         * entonces puede decirse que queda mucho mas entendible.
+         * 
+         */
+        
         // prueba funcional
         $menu = Menu::Builder() // devuelve un nuevo Menu()
-        ->AgregarPri( "Clientes" ) // Devuelve un Pri
-        ->AgregarSub( "Agregar Clientes", "agregar_clientes", [ "Autorizados","Administradores","Operadores" ], "fuente", $funcion )
-        ->buildPri(); // devuelve el Menu actual
+        ->AgregarPrimario( "Clientes" ) // Devuelve un Pri
+        
+        ->AgregarSecundario() 
+        ->nombre( "Agregar Clientes")
+        ->tag( "agregar_clientes" )
+        ->grupos( [ "Autorizados","Administradores","Operadores" ] )
+        ->fuente( "fuente" )
+        ->funcion( $funcion )->buildOpcion()
+        
+        ->AgregarSecundario()
+        ->nombre( "Modificar Clientes")
+        ->tag( "modi_clientes" )
+        ->grupos( [ "Autorizados","Administradores","Operadores" ] )
+        ->fuente( "fuente" )
+        ->funcion( $funcion )->buildOpcion()
+        
+        ->buildMenu();
         
         $menu->ejecutar( "agregar_clientes" );
     }
 
-    function test_New(){
+    
+    function test_ejecutar_fail(){
+        $funcion = function() {
+            $this->assertTrue( true );
+        };
+        
         /*
-         * la idea de esta clase de objeto es establecer una relacion 
-         * entre grupos de usuarios y nobmres de opciones a ejecutar
-         * 
-         * creo que es preferible generar una herramienta que permita informar 
-         * que opciones tiene que grupos 
-         * y que grupos tiene que opciones
-         * 
-         * estoy pensando en un archivo de texto
-         * que contenga: nombre de menu principal
-         * nombre submenu + opcion + grupos habilidatos con comas
-         * 
+         * HONESTAMENTE queda mucho mas largo
+         * pero si la idea es comunicar con claridad,
+         * entonces puede decirse que queda mucho mas entendible.
+         *
          */
-
-        $funcion = function(){ $this->assertTrue(true); };
         
         // prueba funcional
         $menu = Menu::Builder() // devuelve un nuevo Menu()
-        ->AgregarPri( "Clientes" ) // Devuelve un Pri
-        ->AgregarSub( "Agregar Clientes", "agregar_clientes", [ "Autorizados","Administradores","Operadores" ], "fuente", $funcion )
-        ->AgregarSub( "Modificar Clientes", "modificar_clientes", [ "Autorizados","Administradores","Operadores" ] , "fuente", $funcion )
-        ->AgregarSub( "Borrar Clientes", "borrar_clientes", [ "Autorizados","Administradores","Operadores" ] , "fuente", $funcion )
-        ->buildPri() // devuelve el Menu actual
-        ->AgregarPri( "Proveedores" ) 
-        ->AgregarSub( "Agregar Proveedores", "agregar_prov", [ "Autorizados","Administradores","Operadores" ] , "fuente", $funcion )
-        ->AgregarSub( "Modificar Proveedores", "modificar_prov", [ "Autorizados","Administradores","Operadores" ] , "fuente", $funcion )
-        ->AgregarSub( "Borrar Proveedores", "borrar_prov", [ "Autorizados","Administradores","Operadores" ] , "fuente", $funcion )
-        ->buildPri();
+        ->funcionDefault( $funcion )
+        ->AgregarPrimario( "Clientes" ) // Devuelve un Pri
         
-        $menu->ejecutar( "borrar_prov" );
+        ->AgregarSecundario()
+        ->nombre( "Agregar Clientes")
+        ->tag( "agregar_clientes" )
+        ->grupos( [ "Autorizados","Administradores","Operadores" ] )
+        ->fuente( "fuente" )
+        ->funcion( $funcion )->buildOpcion()
         
-//         $json = json_encode( $arr );
-//         $esperado = '{"Clientes":{"Agregar Clientes":"agregar_clientes","Modificar Clientes":"modificar_clientes","Borrar Clientes":"eliminar_clientes"}}';
-//         $actual = $json;
+        ->AgregarSecundario()
+        ->nombre( "Modificar Clientes")
+        ->tag( "modi_clientes" )
+        ->grupos( [ "Autorizados","Administradores","Operadores" ] )
+        ->fuente( "fuente" )
+        ->funcion( $funcion )->buildOpcion()
         
-        // por ahora lo dejo asi
-        $this->assertTrue( true );
+        ->buildMenu();
+        
+        $menu->ejecutar( "nada" );
+    }
+    
+
+    function test_CargarFuente(){
+        
+        // contiene muy_dummy() que devuelve true
+        $archivo = "seguridad_usuarios/dummy.php";
+        $tag = "super_dummy";
+        
+        
+        $menu = Menu::Builder() // devuelve un nuevo Menu()
+        ->AgregarPrimario( "Clientes" ) // Devuelve un Pri
+        
+        ->AgregarSecundario()
+        ->nombre( "Agregar Clientes" )
+        ->tag( $tag )
+        ->grupos( [ "Autorizados","Administradores","Operadores" ] )
+        ->fuente( $archivo )
+        ->funcion( function() { $this->assertTrue( muy_dummy() ); } )
+        ->buildOpcion()
+        ->buildMenu();
+        
+        
+ 
+        $this->assertTrue( $menu->existe_fuente( $tag ) );
+        $menu->cargar_archivo( $tag );
+        $this->assertTrue( $menu->ejecutar( $tag ) );
         
         
     }
