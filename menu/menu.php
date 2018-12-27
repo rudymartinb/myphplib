c55b0651e45c14e261e44356fece269ea7092b85
rudymartinb <paraespam541@gmail.com>
2018-12-27 11:45:05 -0300

    refactoring menu, convertido a builder patter como queria desde un primer momento

--- menu/menu.php
+++ menu/menu.php
@@ -12,65 +12,78 @@ namespace menu;
  */
 
 class Menu {
-    private $primarios;
-    private $actual_opcion;
-    private $actual_subopcion;
-
-    private $tags;
+    protected $primarios;
+    protected $actual_opcion;
+    protected $actual_subopcion;
+    protected $defaultFun;
+    protected $tags;
     
-    private function __construct(){
+    protected function __construct(){
         $this->primarios = [];
         $this->submenu = [];
         $this->tags = [];
     }
     
     static function Builder(){
-        $menu = new Menu();
-        return $menu;
-    }
-    function AgregarPrimario( string $opcion ){
-        $this->primarios[ $opcion ] = [];
-        $this->actual_opcion = $opcion;
-        return $this;
-    }
-    function AgregarSecundario( string $subopcion ){
+        return new class() extends Menu {
+            protected $primarios;
+            protected $actual_opcion;
+            protected $actual_subopcion;
+            protected $defaultFun;
+            
+            protected $tags;
+            
+            function AgregarPrimario( string $opcion ){
+                $this->primarios[ $opcion ] = [];
+                $this->actual_opcion = $opcion;
+                return $this;
+            }
+            
+            function AgregarSecundario( string $subopcion ){
+                $this->primarios[ $this->actual_opcion  ][ $subopcion ] = [];
+                $this->actual_subopcion = $subopcion;
+                return $this;
+            }
+            
+            function tag( string $tag ){
+                $this->tags[ $tag ] = [];
+                $this->tag_actual = $tag;
+                $this->primarios[ $this->actual_opcion  ][ $this->actual_subopcion  ]["tag"] = $tag;
+                return $this;
+            }
+            
+            function grupos( Array $grupos ){
+                $this->tags[ $this->tag_actual ]["grupos"] = $grupos;
+                return $this;
+            }
+            
+            function setfuente( string $fuente ){
+                $this->tags[ $this->tag_actual ]["fuente"] = $fuente;
+                return $this;
+            }
+            
+            function setfuncion( Callable $funcion ){
+                $this->tags[ $this->tag_actual ]["funcion"] = $funcion;
+                return $this;
+            }
+            function setfuncionDefault( Callable $funcion ){
+                $this->defaultFun = $funcion;
+                return $this;
+            }
+            function buildMenu( ){
+                $menu = new Menu();
+                $menu->primarios = $this->primarios;
+                $menu->actual_opcion = $this->actual_opcion;
+                $menu->actual_subopcion = $this->actual_subopcion;
+                $menu->tags = $this->tags;
+                $menu->defaultFun = $this->defaultFun ;
+                return $menu;
+            }
+            
+            
+        };
 
-        $this->primarios[ $this->actual_opcion  ][ $subopcion ] = [];
-        $this->actual_subopcion = $subopcion;
-        
-        return $this;
     }
-    function tag( string $tag ){
-        $this->tags[ $tag ] = [];
-        $this->tag_actual = $tag;
-        
-        $this->primarios[ $this->actual_opcion  ][ $this->actual_subopcion  ]["tag"] = $tag;
-        
-        return $this; 
-    }
-    
-    function grupos( Array $grupos ){
-
-        $this->tags[ $this->tag_actual ]["grupos"] = $grupos;
-        return $this;
-    }
-    function setfuente( string $fuente ){
-        $this->tags[ $this->tag_actual ]["fuente"] = $fuente;
-        return $this;
-    }
-
-    function setfuncion( Callable $funcion ){
-        $this->tags[ $this->tag_actual ]["funcion"] = $funcion;
-        return $this;
-    }
-    function setfuncionDefault( Callable $funcion ){
-        $this->defaultFun = $funcion;
-        return $this;
-    }
-    function buildMenu( ){
-        return $this;
-    }
-    
     
 
     function ejecutar( string $tag ){
